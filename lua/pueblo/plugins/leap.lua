51610fbeae7b9e56d848c5cf63fedee930cab11a
Pawel Lysy <pawel.lysy@blacklabel.pl>
2023-12-20 14:09:08 +0100

    improved keymaps of leap

--- lua/pueblo/plugins/leap.lua
+++ lua/pueblo/plugins/leap.lua
@@ -3,7 +3,7 @@ return {
   enabled = true,
   keys = {
     { "s", mode = { "n", "x", "o" }, desc = "Leap forward to" },
-    { "S", mode = { "n", "x", "o" }, desc = "Leap backward to" },
+    { "S", mode = { "n", "o" }, desc = "Leap backward to" },
     { "gs", mode = { "n", "x", "o" }, desc = "Leap from windows" },
   },
   config = function(_, opts)
