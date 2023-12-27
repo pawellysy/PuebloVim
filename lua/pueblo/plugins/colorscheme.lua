cf2ba2f50a56a03fd393f383eacce178da60b37f
Pawel Lysy <pawel.lysy@blacklabel.pl>
2023-12-27 08:33:27 +0100

    improved colorscheme

--- lua/pueblo/plugins/colorscheme.lua
+++ lua/pueblo/plugins/colorscheme.lua
@@ -1,12 +1,42 @@
 return {
-
     "folke/tokyonight.nvim",
-    -- lazy = 'false',
     event = "VimEnter",
-    opts = { style = "moon" },
-    priority = 1000,
-    config = function (opts)
+    config = function ()
+        require("tokyonight").setup({
+            transparent = true,
+            style = "night",
+            on_highlights = function(hl, c)
+                local prompt = "#2d3149"
+                hl.TelescopeNormal = {
+                    bg = c.bg_dark,
+                    fg = c.fg_dark,
+                }
+                hl.TelescopeBorder = {
+                    bg = c.bg_dark,
+                    fg = c.bg_dark,
+                }
+                hl.TelescopePromptNormal = {
+                    bg = prompt,
+                }
+                hl.TelescopePromptBorder = {
+                    bg = prompt,
+                    fg = prompt,
+                }
+                hl.TelescopePromptTitle = {
+                    bg = prompt,
+                    fg = prompt,
+                }
+                hl.TelescopePreviewTitle = {
+                    bg = c.bg_dark,
+                    fg = c.bg_dark,
+                }
+                hl.TelescopeResultsTitle = {
+                    bg = c.bg_dark,
+                    fg = c.bg_dark,
+                }
+            end,
+        })
+
         vim.cmd.colorscheme('tokyonight')
-        require("tokyonight").setup(opts)
     end
 }
