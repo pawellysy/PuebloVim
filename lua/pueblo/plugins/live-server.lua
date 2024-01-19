19e701ed049cf797a699ef6d759b385fbcae720e
Pawel Lysy <pawel.lysy@blacklabel.pl>
2024-01-19 08:37:51 +0100

    cleanup of plugins

--- lua/pueblo/plugins/live-server.lua
+++
@@ -1,9 +0,0 @@
-return {
-    'barrett-ruth/live-server.nvim',
-    build = 'yarn global add live-server',
-    config = true,
-    setup = function()
-        require('live-server').setup()
-    end,
-
-}
--- lua/pueblo/plugins/oil.lua
+++ lua/pueblo/plugins/oil.lua
@@ -1,6 +1,7 @@
 return {
     "stevearc/oil.nvim",
     opts = {},
+    enabled = false,
     config = function()
         require("oil").setup()
     end
