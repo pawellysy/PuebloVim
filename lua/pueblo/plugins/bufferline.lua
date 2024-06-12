8fecb9db098b5055ba0b680f0a12af8d64e1f9c4
Pawel Lysy <pawel.lysy@blacklabel.pl>
2024-06-12 18:49:53 +0200

    Added bufferline for just tabs

--- lua/pueblo/plugins/bufferline.lua
+++ lua/pueblo/plugins/bufferline.lua
@@ -1,9 +1,15 @@
 return {
     'akinsho/bufferline.nvim',
     version = "*",
-    enabled = false,
+    -- enabled = false,
     dependencies = 'nvim-tree/nvim-web-devicons',
     config = function()
-        require('bufferline').setup()
+        require('bufferline').setup({
+            options = {
+                show_buffer_icons = false,
+                mode = 'tabs',
+                always_show_bufferline = false
+            }
+        })
     end
 }
