09ca876697f7d689b70496c8bbdb73bff0f9d45e
plysy_dbx <plysy@dropbox.com>
2025-01-23 14:17:35 +0100

    bqf

---
+++ lua/pueblo/plugins/bqf.lua
@@ -0,0 +1,13 @@
+return {
+    'kevinhwang91/nvim-bqf',
+    event = 'VeryLazy',
+    dependencies = { 'junegunn/fzf' },
+    config = function()
+        require('bqf').setup({
+
+            func_map = {
+                fzffilter = 'zF'
+            }
+        })
+    end
+}
